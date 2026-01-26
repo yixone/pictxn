c3618f04e258dbadd92533110234da242e7c1a0a
Александр <sasha-kvasnikov@mail.ru>
2026-01-26 17:20:10 +0300

    add consts

--- apps/core/src/lib.rs
+++ apps/core/src/lib.rs
@@ -1,1 +1,11 @@
 pub mod helpers;
+
+pub(crate) const MEGABYTES: usize = 1024 * 1024;
+
+pub(crate) const APP_NAME: &str = "pictxn";
+pub(crate) const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub(crate) const DATA_DIR: &str = "/data";
+pub(crate) const MEDIA_DIR: &str = "/media";
+pub(crate) const TEMP_DIR: &str = "/temp";
+pub(crate) const DB_FILE: &str = "/pictxn.db";
