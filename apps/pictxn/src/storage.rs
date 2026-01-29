02a4dd7dc01382b15d5bc85739a7dc78bc02661f
Александр <sasha-kvasnikov@mail.ru>
2026-01-29 19:04:54 +0300

    change StorageProvider types BoxStream -> generics

--- apps/pictxn/src/storage.rs
+++ apps/pictxn/src/storage.rs
@@ -1,17 +1,49 @@
-use futures::stream::BoxStream;
+use std::path::PathBuf;
+
+use futures::Stream;
 use tokio_util::bytes::Bytes;
 
 use crate::types::files::FileWriteResult;
 
-type Stream<'a> = BoxStream<'a, std::io::Result<Bytes>>;
+// type Stream<'a> = BoxStream<'a, std::io::Result<Bytes>>;
+
+pub struct FileReadResult<S>
+where
+    S: Stream<Item = std::io::Result<Bytes>>,
+{
+    pub stream: S,
+}
 
 #[async_trait::async_trait]
 pub trait StorageProvider {
-    async fn set<'a>(&self, key: &str, stream: Stream<'a>) -> std::io::Result<FileWriteResult>;
+    async fn set<S>(&self, key: &str, stream: S) -> std::io::Result<FileWriteResult>
+    where
+        S: Stream<Item = std::io::Result<Bytes>> + Send + Unpin;
 
-    async fn get(&self, key: &str) -> std::io::Result<Stream<'static>>;
+    async fn get(
+        &self,
+        key: &str,
+    ) -> std::io::Result<impl Stream<Item = std::io::Result<Bytes>> + Send>;
 
     async fn mv(&self, from: &str, to: &str) -> std::io::Result<()>;
 
     async fn del(&self, key: &str) -> std::io::Result<()>;
+}
+
+/// Implementation of file storage that works with the server file system
+pub struct FsStorage {
+    root: PathBuf,
+}
+
+impl FsStorage {
+    pub fn init(root: impl Into<PathBuf>) -> std::io::Result<Self> {
+        let root = root.into();
+        std::fs::create_dir_all(&root)?;
+
+        Ok(FsStorage { root })
+    }
+
+    fn generate_path_from_key(&self, key: &str) -> PathBuf {
+        self.root.join(&key[..2]).join(&key[2..4]).join(key)
+    }
 }
