1c70cbf651b3940591ebb89b4e34a88f32cfc4da
Александр <sasha-kvasnikov@mail.ru>
2026-01-30 23:39:32 +0300

    add simple card id generator

--- apps/pictxn/src/models/domains/cards.rs
+++ apps/pictxn/src/models/domains/cards.rs
@@ -65,7 +65,19 @@ pub struct CardId(i64);
 
 impl CardId {
     pub fn generate() -> Self {
-        todo!()
+        const TS_MASK: i64 = (1i64 << 42) - 1;
+        const PID_MASK: i64 = (1 << 8) - 1;
+        const RN_MASK: i64 = (1 << 13) - 1;
+
+        const TS_OFFSET: i64 = 1750000000000;
+
+        let ts = (Utc::now().timestamp_millis() - TS_OFFSET) & TS_MASK;
+        let pid = process::id() as i64 & PID_MASK;
+        let rn = (rng().random::<u32>() as i64) & RN_MASK;
+
+        let id = (ts << (8 + 13)) | (pid << 13) | rn;
+
+        CardId(id)
     }
 }
 
