d8387f63209a15e6815de645f36fe68ebf769e4b
Александр <sasha-kvasnikov@mail.ru>
2026-01-30 19:57:58 +0300

    add repositories base

--- apps/pictxn/src/repositories/container.rs
+++ apps/pictxn/src/repositories/container.rs
@@ -1,9 +1,41 @@
 use sqlx::SqlitePool;
 
-pub struct RepositoriesContainer {}
+use crate::repositories::{
+    cards::CardsRepository, files::FilesRepository, profiles::ProfilesRepository,
+    users::UsersRepository,
+};
+
+pub struct RepositoriesContainer {
+    cards: CardsRepository,
+    files: FilesRepository,
+
+    profiles: ProfilesRepository,
+    users: UsersRepository,
+}
 
 impl RepositoriesContainer {
     pub fn new(pool: SqlitePool) -> Self {
-        RepositoriesContainer {}
+        RepositoriesContainer {
+            cards: CardsRepository::new(pool.clone()),
+            files: FilesRepository::new(pool.clone()),
+            profiles: ProfilesRepository::new(pool.clone()),
+            users: UsersRepository::new(pool),
+        }
+    }
+
+    pub fn cards(&self) -> &CardsRepository {
+        &self.cards
+    }
+
+    pub fn files(&self) -> &FilesRepository {
+        &self.files
+    }
+
+    pub fn profiles(&self) -> &ProfilesRepository {
+        &self.profiles
+    }
+
+    pub fn users(&self) -> &UsersRepository {
+        &self.users
     }
 }
