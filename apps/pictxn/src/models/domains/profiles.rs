c8b5bc771ecc23c20b6196e83715bfb7f7e0f12d
Александр <sasha-kvasnikov@mail.ru>
2026-01-31 16:56:16 +0300

    add profile domain

--- apps/pictxn/src/models/domains/profiles.rs
+++ apps/pictxn/src/models/domains/profiles.rs
@@ -1,3 +1,21 @@
+use crate::models::domains::{files::FileId, users::UserId};
+
 pub struct ProfileDomain {
-    //
+    id: UserId,
+
+    display_name: String,
+
+    avatar_id: Option<FileId>,
+    banner_id: Option<FileId>,
+}
+
+impl ProfileDomain {
+    pub fn new(id: UserId, display_name: String) -> Self {
+        ProfileDomain {
+            id,
+            display_name,
+            avatar_id: None,
+            banner_id: None,
+        }
+    }
 }
