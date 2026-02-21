f649e68a4f607588ae11ac2cb0bd6526c80ee5ce
Александр <sasha-kvasnikov@mail.ru>
2026-02-21 11:22:47 +0300

    add base modules

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+/// External APIs content aggregator
+pub mod scout;
+
+/// Result and Error types
+pub mod result;
+
+/// Domain models and types
+pub mod domains;
+
+/// Database module
+pub mod database;
+
+/// Utilities
+pub mod util;
+
+/// DTO and API projections
+pub mod api;
+
+/// Business logic and use cases
+pub mod services;
+
+/// File storage services
+pub mod storage;
